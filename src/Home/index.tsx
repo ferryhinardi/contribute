ac88294557ea0547268b46f3a355b6add6f4d4a3
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2020-12-21 21:22:44 +0700

    add google signin feature

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { GoogleSigninButton } from '@react-native-community/google-signin';
+import useAuth from '../hooks/useAuth';
 import { HomeScreenNavigationProp } from '../../types';
 
 function HomeScreen({ navigation }: { navigation: HomeScreenNavigationProp }) {
+  const { user, signInWithGoogle, signOut } = useAuth();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+      {user ? <Text>{`Welcome, ${user?.displayName}`}</Text> : <Text>Home Screen</Text>}
+      {!user ? (
+        <>
+          <GoogleSigninButton
+            style={{ width: 192, height: 48 }}
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={signInWithGoogle}
+          />
+        </>
+      ): (
+        <>
+          <Button
+            title="Go to Details"
+            onPress={() => navigation.navigate('Details')}
+          />
+          <Button
+            title="Sign out"
+            onPress={signOut}
+          />
+        </>
+      )}
     </View>
   );
 }
---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import { GoogleSignin, statusCodes } from '@react-native-community/google-signin';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
+function useAuth() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  function onAuthStateChanged(user: any) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['email'],
+      webClientId: '250364316517-t02v4c9opkvrbvobedi07hem939v0fkt.apps.googleusercontent.com',
+      offlineAccess: true
+    });
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  const signInWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      // Get the users ID token
+      const user = await GoogleSignin.signIn();
+      // Create a Google credential with the token
+      const googleCredential = auth.GoogleAuthProvider.credential(user.idToken);
+      // Sign-in the user with the credential
+      return auth().signInWithCredential(googleCredential);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        console.log('Cancel');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('Signin in progress');
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('PLAY_SERVICES_NOT_AVAILABLE');
+        // play services not available or outdated
+      } else {
+        // some other error happened
+        console.log('Other Error...', JSON.stringify(error));
+      }
+    }
+  };
+  const signOut = async () => {
+    const res = await auth().signOut();
+    return res;
+  };
+
+  return {
+    initializing,
+    user,
+    signInWithGoogle,
+    signOut,
+  };
+}
+
+export default useAuth;
